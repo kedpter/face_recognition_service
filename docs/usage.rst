e86b39f8396ec0effc5bdc6a3a50a728c495c874
kedpter <790476448@qq.com>
2020-08-10 10:45:26 +0800

    do not use waitress start

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use face_recognition_service in a project::
-
-    import face_recognition_service
--- face_recognition_service/cli.py
+++ face_recognition_service/cli.py
@@ -3,20 +3,18 @@ from face_recognition_service.face_engine import FaceEngine
 import os
 
 
-# def main():
-#     app.debug = True
-#     # FaceEngine().debug = True
-#     # app.config['DATABASE_NAME'] = 'library.db'
-#     host = os.environ.get('IP', '0.0.0.0')
-#     port = int(os.environ.get('PORT', 8080))
-#     app.run(host=host, port=port)
+def main():
+     app.debug = True
+     # FaceEngine().debug = True
+     # app.config['DATABASE_NAME'] = 'library.db'
+     host = os.environ.get('IP', '0.0.0.0')
+     port = int(os.environ.get('PORT', 8080))
+     app.run(host=host, port=port)
 #
 #
 # if __name__ == '__main__':
 #     main()
-def main():
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+
 
 
 if __name__ == "__main__":
